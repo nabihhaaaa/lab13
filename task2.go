9b4664778b9266d57da12a8b38cf4b449e31901b
nabihhaaaa <114142426+nabihhaaaa@users.noreply.github.com>
2022-11-22 22:13:00 +0500

    Add files via upload

---
+++ task2.go
@@ -0,0 +1,49 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+//task 2
+
+import "fmt"
+
+type emp struct {
+	name     string
+	salary   int
+	position string
+}
+type company struct {
+	companyName string
+	empls        []emp
+}
+
+func main() {
+	var comp company
+
+	e1 := emp{"Asif", 80000, "Cybersecurity Analyst"}
+	e2 := emp{"Umer", 40000, "Data Scientist"}
+	e3 := emp{"Hamza", 50000, "Content Creator"}
+
+	emplys := []emp{e1, e2, e3}
+
+	comp.companyName = "FAST Techz"
+	comp.empls = emplys
+
+	fmt.Println("Company: ", comp.companyName)
+	fmt.Print("Employees are: \n\n")
+
+	fmt.Print("Employee1: \n")
+	fmt.Println("Name: ", comp.empls[0].name)
+	fmt.Println("Salary: ", comp.empls[0].salary)
+	fmt.Println("Position: ", comp.empls[0].position)
+
+	fmt.Print("\nEmployee2: \n")
+	fmt.Println("Name: ", comp.empls[1].name)
+	fmt.Println("Salary: ", comp.empls[1].salary)
+	fmt.Println("Position: ", comp.empls[1].position)
+
+	fmt.Print("\nEmployee3 \n")
+	fmt.Println("Name: ", comp.empls[2].name)
+	fmt.Println("Salary: ", comp.empls[2].salary)
+	fmt.Println("Position: ", comp.empls[2].position)
+
+}
